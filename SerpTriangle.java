e973bf1631b968c34e012cb501303113f2763e48
DPS100 <0dps100@gmail.com>
2020-02-28 06:55:24 -0700

    Update SerpTriangle.java

--- SerpTriangle.java
+++ SerpTriangle.java
@@ -1,6 +1,6 @@
 public class SerpTriangle extends Shape{
 
-    public int sides;
+    public int generations;
     private Point[] midpoints;
 
     public SerpTriangle(Point[] points) {
@@ -9,8 +9,15 @@ public class SerpTriangle extends Shape{
     }
 
     public void calculateMidpoints() {
-        for(int x = 0; x < super.getSides(); x++) {
-            
+        int halfX;
+        int halfY;
+        for(int x = 0; x < super.getSides() - 1; x++) {
+            halfX = (super.getPoint(x).getXCoord() + super.getPoint(x + 1).getXCoord()) / 2;
+            halfY = (super.getPoint(x).getYCoord() + super.getPoint(x + 1).getYCoord()) / 2;
+            midpoints[x] = new Point(halfX, halfY);
         }
+        halfX = (super.getPoint(0).getXCoord() + super.getPoint(super.getSides() - 1).getXCoord()) / 2;
+        halfY = (super.getPoint(0).getYCoord() + super.getPoint(super.getSides() - 1).getYCoord()) / 2;
+        midpoints[super.getSides() - 1] = new Point(halfX, halfY);
     }
 }
